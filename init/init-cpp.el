a001a5d00fb60a2f82cc11eaaf659d39f5dfd091
Evan Bergeron <evan@memsql.com>
2018-10-18 01:30:07 -0700

    Make life way better in cpp land

--- init/init-cpp.el
+++ init/init-cpp.el
@@ -4,9 +4,18 @@
    ((looking-at "\\.")   (replace-string "." "->" nil (point) (+ (point) 1)))
    ((looking-at "\\->")  (replace-string "->" "." nil (point) (+ (point) 2)))))
 
-(provide 'init-cpp)
-
 (evil-leader/set-key
   "o" 'c-toggle-dot-pointer
   "h" 'ff-find-other-file
   "l" 'clang-format-region)
+
+(setq-default c-basic-offset 4)
+(setq c-default-style "linux")
+
+(add-to-list 'auto-mode-alist '("\\.mpl\\'" . c++-mode))
+
+(add-hook 'c-mode-common-hook '(lambda () (c-toggle-auto-state 1)))
+
+(define-key c++-mode-map [C-down-mouse-1] 'rtags-find-symbol-at-point)
+
+(provide 'init-cpp)
