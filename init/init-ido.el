f46faac12687c08d43f60944e48e68072d7908b3
Evan Bergeron <evan@memsql.com>
2019-03-08 11:12:00 -0800

    Map page-down to quit ido grid

--- init/init-ido.el
+++ init/init-ido.el
@@ -2,5 +2,12 @@
 (require 'ido-grid-mode)
 (ido-mode t)
 (ido-grid-mode t)
+(ido-everywhere 1)
+
+(defun bind-ido-keys ()
+  "Keybindings for ido mode."
+  (define-key ido-completion-map (kbd "<next>") 'keyboard-escape-quit))
+
+(add-hook 'ido-setup-hook #'bind-ido-keys)
 
 (provide 'init-ido)
