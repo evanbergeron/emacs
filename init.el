e7f2e0e99c9197ec477a587e3774c825b70078a1
Evan Bergeron <evan@memsql.com>
2019-05-23 23:08:52 -0700

    one escape to exit minibuffer + smex and rust inits

--- init.el
+++ init.el
@@ -13,13 +13,16 @@
 (require 'init-basics)
 (require 'init-evil)
 (require 'init-ido)
+(require 'init-smex)
 (require 'init-windowing)
 (require 'init-visual)
 (require 'init-rtags)
 (require 'init-cpp)
+(require 'init-rust)
 (require 'init-memsql)
 (require 'init-magit)
 (require 'init-flycheck)
+(require 'init-minibuffer)
 
 (setq inhibit-splash-screen t)
 ;; (switch-to-buffer "~/.emacs.splash")
@@ -28,6 +31,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e8ebf4fb99b76dad3b7ec2313d5d091c1f3a3e0e4f9d55d3a58d73a8d3387358" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(package-archives
    (quote
     (("marmalade" . "http://marmalade-repo.org/packages/")
@@ -37,7 +43,8 @@
      ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (general ido-completing-read+ flycheck magit solarized-theme rainbow-mode magit-popup key-chord ido-grid-mode git-commit ghub evil-numbers evil-leader evil-commentary dracula-theme clang-format atom-one-dark-theme))))
+    (simplenote2 smex general ido-completing-read+ flycheck magit solarized-theme rainbow-mode magit-popup key-chord ido-grid-mode git-commit ghub evil-numbers evil-leader evil-commentary dracula-theme clang-format atom-one-dark-theme)))
+ '(pdf-view-midnight-colors (quote ("#c5c8c6" . "#232c31"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ init/init-minibuffer.el
@@ -0,0 +1,2 @@
+(define-key minibuffer-local-map (kbd "<escape>") 'keyboard-escape-quit)
+(provide 'init-minibuffer)
